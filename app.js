4c4d8ea03d06d08ada8e8703695bf73daff160e2
Jathusan-Jesuthasan <jesuthasanjathusan@gmail.com>
2026-02-21 23:42:02 +0530

    Added read file example

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Lab 04 - NodeJS Setup Complete");
+const fs = require('fs');
+
+fs.readFile('file.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
